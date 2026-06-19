9c916a9825c7249a6f155b2dabe8632b76a46ab2
Jack Lucas <jackallen2024@gmail.com>
2026-06-19 18:15:31 -0400

    finished single verse endpoint, stubbed out verse range and whole book reference points

--- apps/api/src/routers/reference.ts
+++ apps/api/src/routers/reference.ts
@@ -3,6 +3,24 @@ import { publicProcedure, router } from '../trpc'
 import { TRPCError } from '@trpc/server'
 
 export const referenceRouter = router({
+
+    // retrieve an entire book's contents at once, with format
+    getBook: publicProcedure
+        .input(
+            z.object({
+                book_id: z.string(),
+                translation_id: z.string().default('NABRE')
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const { book_id, translation_id } = input
+
+
+
+            return {}
+        }),
+
+    // retrieve an entire chapter at once, with formatted contents
     getChapter: publicProcedure
         .input(
             z.object({
@@ -63,5 +81,68 @@ export const referenceRouter = router({
                             : null
                 }))
             }
+
+        }),
+
+    // retrieve a single verse contents at once
+    getVerse: publicProcedure
+        .input(
+            z.object({
+                book_id: z.string(),
+                chapter_number: z.number().int().positive(),
+                verse_number: z.number().int().positive(),
+                translation_id: z.string().default('NABRE')
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const { book_id, chapter_number, verse_number, translation_id } = input
+
+            const verse = await ctx.db.verse.findUnique({
+                where: {
+                    number_chapter_number_book_id_translation_id: {
+                        number: verse_number,
+                        chapter_number: chapter_number,
+                        book_id: book_id,
+                        translation_id: translation_id
+                    }
+                },
+                include: {
+                    chapter: {
+                        include: {
+                            book: true
+                        }
+                    }
+                }
+            })
+
+            if (!verse) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: `${book_id} ${chapter_number}:${verse_number} not found in ${translation_id}`
+                })
+            }
+
+            return verse
+
+        }),
+
+    // return a range of verses
+    getVerseRange: publicProcedure
+        .input(
+            z.object({
+                book_id: z.string(),
+                chapter_start: z.number().int().positive(),
+                chapter_end: z.number().int().positive(),
+                verse_start: z.number().int().positive(),
+                verse_end: z.number().int().positive(),
+                translation_id: z.string().default('NABRE')
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const { book_id, chapter_start, chapter_end, verse_start, verse_end, translation_id } = input
+
+            
+
+            return {}
         })
 })
