090193c9b40a01df5be281ae4e54fa1be6686561
Jack Lucas <jackallen2024@gmail.com>
2026-06-28 21:46:58 -0400

    initialized shadcn/ui pre-built components tool with tailwind CSS

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,9 @@
 import { TRPCProvider } from '@/lib/trpc/provider'
+import { JetBrains_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+
 
 export default function RootLayout({
     children,
@@ -6,7 +11,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className={cn("font-mono", jetbrainsMono.variable)}>
             <body>
                 <TRPCProvider>
                     {children}
