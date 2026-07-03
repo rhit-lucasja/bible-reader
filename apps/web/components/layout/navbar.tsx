391b4a8736993404fa41a1f31d9b624a4cd39b94
Jack Lucas <jackallen2024@gmail.com>
2026-07-02 23:52:09 -0400

    rearranging some icons in navbar

--- apps/web/components/layout/navbar.tsx
+++ apps/web/components/layout/navbar.tsx
@@ -1,5 +1,5 @@
 import { auth, signOut } from '@/auth';
-import { BookOpenTextIcon, SearchIcon } from 'lucide-react';
+import { BookOpenTextIcon, FlameIcon, SearchIcon } from 'lucide-react';
 
 export default async function Navbar() {
     const session = await auth()
@@ -7,15 +7,16 @@ export default async function Navbar() {
     return (
         <nav className="fixed top-0 left-0 w-full h-12 bg-blue-900 text-white text-2xl font-bold flex gap-8 pl-2 py-2 z-50">
             <a href="/" className="flex gap-2">
-                <BookOpenTextIcon className="h-9 w-auto" />
+                <FlameIcon className="h-8 w-auto" />
                 Ignis Divinus
             </a>
-            <a href="/read/GEN/1">
-                Read the Bible
+            <a href="/read/GEN/1" className="flex gap-2">
+                <BookOpenTextIcon className="h-9 w-auto" />
+                Read
             </a>
             <a href="/search" className="flex gap-2">
-                Search
                 <SearchIcon className="h-7 w-auto" />
+                Search
             </a>
             {session ? (
                 <form action={async () => { 'use server'; await signOut({ redirectTo: '/' }) }}>
